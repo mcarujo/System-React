db0c808b4ad90a49855efb9acf17dc33ae588f05
Marco Carujo <mamcarujo@gmail.com>
2019-06-16 15:53:19 -0300

    recreating the framework core

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
+import { TableGeneric } from "./screen/TableGeneric";
+import { FormGeneric } from "./screen/FormGeneric";
 import * as serviceWorker from "./serviceWorker";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={App} />
+      <Route path="/table" component={TableGeneric} />
+      <Route path="/form" component={FormGeneric} />
+    </Route>
+  </Router>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
